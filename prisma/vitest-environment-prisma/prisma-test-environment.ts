c141a708c9280068979e190ca5063f8b69206c33
Daniel Borges <danprog91@gmail.com>
2024-03-13 06:41:03 -0300

    Problema nos testes e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -38,6 +38,7 @@ export default <Environment>{
         await prisma.$executeRawUnsafe(
           `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
         )
+
         await prisma.$disconnect()
       },
     }
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,7 +1,7 @@
+import { UsersRepository } from '@/http/repositories/users-repository'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists'
-import { User } from '@prisma/client'
-import { UsersRepository } from '@/http/repositories/users-repository'
 
 interface RegisterUseCaseRequest {
   name: string
