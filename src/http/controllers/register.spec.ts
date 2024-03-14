c7eb8503b5239c5e4028502effbafb3500e658d4
Daniel Borges <danprog91@gmail.com>
2024-03-14 20:04:25 -0300

    Teste E2E do registro

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,5 @@
-import request from 'supertest'
 import { app } from '@/app'
+import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Register (e2e)', () => {
@@ -14,7 +14,7 @@ describe('Register (e2e)', () => {
   it('should be able to register', async () => {
     const response = await request(app.server).post('/users').send({
       name: 'John Doe',
-      email: 'johndoe2031@example.com',
+      email: 'johndoe@example.com',
       password: '123456',
     })
 
